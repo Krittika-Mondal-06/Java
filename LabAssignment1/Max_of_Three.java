a9b57a6deb9f8beff47ecd811aca9769222a320d
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2025-03-04 20:39:40 +0530

    Another Code

--- LabAssignment1/Max_of_Three.java
+++ LabAssignment1/Max_of_Three.java
@@ -3,15 +3,15 @@ public class Max_of_Three
 {
     public static void main(String args[])
     {
-        Scanner myObj=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         System.out.println("*Program for finding maximum of three numbers.*\n");
         System.out.println("Enter the three numbers you want to compare:");
         System.out.print("1st Number:");
-        float num1 = myObj.nextFloat();
+        float num1 = sc.nextFloat();
         System.out.print("2nd Number:");
-        float num2 = myObj.nextFloat();
+        float num2 = sc.nextFloat();
         System.out.print("3rd Number:");
-        float num3 = myObj.nextFloat();
+        float num3 = sc.nextFloat();
         if(num1>=num2 && num1>=num3)
         System.out.println(num1 + " is the maximum number.");
         else if(num2>=num1&&num2>=num3)

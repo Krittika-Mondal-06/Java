c4dd70e44f9ec86956ce1d9ec6b850576b57fdac
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2025-03-06 12:57:02 +0530

    Another Code

---
+++ LabAssignment7/MultiCatch.java
@@ -0,0 +1,11 @@
+class MultiCatch {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+            int arr[] = new int[5];
+            arr[10] = 50;
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught: " + e);
+        }
+    }
+}

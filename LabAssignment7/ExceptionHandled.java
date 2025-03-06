07370083a046e9515972f3a7a2c4c7b129cf3ec9
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2025-03-06 11:05:55 +0530

    Another Code

---
+++ LabAssignment7/ExceptionHandled.java
@@ -0,0 +1,11 @@
+public class ExceptionHandled {
+    public static void main(String[] args) {
+        try {
+            int a = 10, b = 0;
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}

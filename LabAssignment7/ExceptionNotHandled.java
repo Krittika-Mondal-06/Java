84893edf6571880bef7c4cd81a8eec0eba83a75a
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2025-03-06 12:19:53 +0530

    Another Code

---
+++ LabAssignment7/ExceptionNotHandled.java
@@ -0,0 +1,7 @@
+class ExceptionNotHandled {
+    public static void main(String[] args) {
+        int a = 10, b = 0;
+        int result = a / b; // This will crash the program
+        System.out.println("Result: " + result);
+    }
+}
